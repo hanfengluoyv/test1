1fa38fc2ac0a84b007efab069d423ae0c396d367
Administrator <Administrator@JQJLJDG6B4V3H09>
2019-08-27 14:47:40 +0800

    sp01-commons 提交gitbub

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/User.java
@@ -0,0 +1,14 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {//用户
+	private Integer id;    
+	private String username; //用户名
+	private String password; //密码
+}
